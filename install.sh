669d3fc9b3c911524549d923f850339311d1c52a
Ruohan Zhan <ruohanzhan@gmail.com>
2023-02-08 09:47:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 python setup.py develop
 conda install -c conda-forge rpy2
 conda install -c conda-forge r-devtools r-grf r-bh
-Rscript -e 'install.packages("policytree", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("policytree")'
+
