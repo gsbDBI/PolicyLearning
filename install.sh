6bddcc53a78ed9679e6128713c7391ccb09ed9f0
Ruohan Zhan <ruohanzhan@gmail.com>
2023-02-08 10:41:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 python setup.py develop
-conda install -c conda-forge rpy2
+conda install -c anaconda jupyter
+conda install -c conda-forge rpy2 openml
 conda install -c conda-forge r-devtools r-grf r-bh
 
